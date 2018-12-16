09a1d30362492e3183475c3cfe5b016d4de5e37f
Brandon Porter <brandon.porter18@gmail.com>
2018-12-16 13:30:23 -0800

    Format empty div text

--- src/web/components/video-downloads/EmptyDisplay.jsx
+++ src/web/components/video-downloads/EmptyDisplay.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export const EmptyDisplay = () => <div className="empty-text">Add a URL and select "Download" to begin downloading!</div>
+export const EmptyDisplay = () => (
+    <div className="empty-display">
+        <div className="empty-display__text">Add a Youtube URL and press <span className="bold">Download</span> to convert it to an MP3</div>
+    </div>
+);
